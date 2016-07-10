0147dba17c45b6e0286a1e87109e63e17232a41f
mizutombo <thomas.shultz52@gmail.com>
2016-07-10 13:45:34 -0700

    prep for roll to branch class05 ... changed all variable names to camel-case ... eliminated excessive muted messages to user

--- gamefunction.js
+++ gamefunction.js
@@ -1,111 +1,85 @@
 
-//function "gameMe" to run code for guessing game
-function gameMe() {
+//function "gameme" to run code for guessing game
+function gameme() {
 
-  /*Revised code for opening question to user to account for user not providing userName, or hitting "Cancel" button when asked for userName.*/
   var userName = "";
-  //Establish greeting function.
   while ((userName == "") && (userName != null)) {
-  //System continues to badger user for userName until a name is provided.
   userName = prompt("What is your name?", "");
   }
   if ((userName != null) && (userName != "")) {
-    //Opening greeting to user.
     alert("Very nice to meet you " +userName+ "! \nHere is my guessing game.");
-    //System response if user hits "Cancel" button.
   } else {
-    //System responds and quits asking questions if user hits 'Cancel' button.
     alert("Okay, if you're not interested, then goodbye.");
   }
   /*Insert "if" statement code to enable system to stop if user hits "Cancel" button at any time during questioning.*/
   if (userName != null) {
-      //Initialize score counter at 0
-      var Count = 0;
-      //Set parameters for 1st question.
+      var count = 0;
       query ("Did Tom grow up in Portland, Oregon?  Yes or No. ", "  Correct, " +userName+ "!  Tom grew up in Seattle, Washington.", "  Wrong, " +userName+ "!  Tom didn't move to Portland until 1994!");
-      //Set parameters for 2nd question.
       query ("Did Tom attend Washington State University?  Yes or No. ", "  Correct, " +userName+ "! Tom went to the University of Washington. Go Huskies!!!", "  Arrgh! Wrong, " +userName+ "!, Tom is a UW Husky, not a WSU Cougar.");
-      //Set parameters for 3rd question.
       query ("Does Tom really want to work for another large technology company after he completes his Code 401 class? Yes or No. ", "  Correct, " +userName+ "! Tom wants to work for a small to mid-size software development company.", "  Wrong, " +userName+ "! Never ever again does Tom want to work for a large company.");
 
-      //Establish function 'query' to run code for Y/N guessing game questions.
-      function query(question, posfeed, negfeed) {
+      //YES/NO guessing game with 3 questions.
+      function query(question, posFeed, negFeed) {
         var userRsp = prompt(question);
-        //set var to enable code to normalize userRsp as upper case regardless of response case
         var areEqual = userRsp.toUpperCase();
         console.log(userRsp);
         console.log(areEqual);
-        //Establish variable 'ynquest' for Y/N question loop.
-        var ynquest = '';
-        //Establish 'if' 'else' loop for Y/N question loop.
-        //areEqual = normalized userResp
+        var ynQuest = '';
         if ((areEqual == "NO") || (areEqual == "N")) {
           console.log(areEqual);
-          // Question + feedback for correct answer.
-          ynquest = question + posfeed + '<br/>' + '<br/>';
-          console.log(ynquest);
-          // Add 1 point to score for correct answer.
-          Count ++;
+          ynQuest = question + posFeed + '<br/>' + '<br/>';
+          console.log(ynQuest);
+          count ++;
         } else {
-          // Question + feedback for wrong answer.
-          ynquest = question + negfeed + '<br/>' + '<br/>';
-          console.log(ynquest);
+          ynQuest = question + negFeed + '<br/>' + '<br/>';
+          console.log(ynQuest);
         }
-        // Sequentially write questions & feedback to DOM without overwrite.
-        document.getElementById('scribe1').innerHTML += ynquest;
+        document.getElementById('scribe1').innerHTML += ynQuest;
       };
 
       //Numerical years worked guessing game, with 3 as correct answer.
       var correct = 3;
-      //Set parameters for 'yearguess' function.
       yearguess ("For how many years did Tom work for Intel?", " Yes, you are correct! 3 years with Intel.", " Sorry,your guess is too high ... and Tom is not that crazy.", " Sorry, your guess is too low.");
 
-      //Established function 'yearguess' for years worked guessing game.
-      function yearguess(queryyears, cormsg, highmsg, lowmsg) {
-        var userYearGuess = prompt(queryyears);
+      function yearguess(queryYears, corMsg, highMsg, lowMsg) {
+        var userYearGuess = prompt(queryYears);
         //Establish 'if' 'else if' 'else' loop for query.
         if (userYearGuess > correct) {
           //User guesses number greater than 3 ... which is too high.
           //Write question & feedback to DOM for answer that is too high.
-          document.getElementById('scribe2').innerHTML += queryyears.concat(highmsg);
+          document.getElementById('scribe2').innerHTML += queryYears.concat(highMsg);
         } else if (userYearGuess == correct) {
           //User guesses the answer of 3 ... which is correct.
           //Write question & feedback to DOM for correct answer.
-          document.getElementById('scribe2').innerHTML += queryyears.concat(cormsg);
+          document.getElementById('scribe2').innerHTML += queryYears.concat(corMsg);
           // Add 1 point to score for correct answer.
-          Count ++;
+          count ++;
         } else {
           //User guesses number less than 3 ... which is too low.
           //Write question & feedback to DOM for answer that is too low.
-          document.getElementById('scribe2').innerHTML += queryyears.concat(lowmsg);
+          document.getElementById('scribe2').innerHTML += queryYears.concat(lowMsg);
         }
       };
 
-      //Number guessing game with 5 iterations.
+      //Lucky number guessing game with 5 iterations.
       alert("Now let's play a numbers guessing game. \nI'll give you 5 tries.");
-      //Set parameters for 'guessnum' function
       guessnum ("Guess my lucky number between 1 and 10.", " Correct ... my lucky number is 7.", " Wrong answer. Try again.", " Sorry, but you've used up your 5 tries.");
 
-      //Establish function 'guessnum' for number guessing game.
-      function guessnum(querynum, cornbrmsg, wrgnbrmsg, limit) {
-        var number = prompt(querynum);
-        //Establish 'for' loop to enable maximum 5 guesses.
-        for (var guesstry = 1; guesstry < 5; guesstry++) {
+      function guessnum(queryNum, corNbrMsg, wrgNbrMsg, limit) {
+        var number = prompt(queryNum);
+        for (var guessTry = 1; guessTry < 5; guessTry++) {
           if (number != 7) {
-            //If user guess is not the number 7, then the loop will continue to prompt the user for another number guess until the guess limit is reached.
-            var number = prompt(wrgnbrmsg);
-            document.getElementById('scribe4').innerHTML = querynum.concat(limit);
+            var number = prompt(wrgNbrMsg);
+            document.getElementById('scribe4').innerHTML = queryNum.concat(limit);
           } else {
-            //If user correctly guesses the number 7, then write question & feedback to DOM, and end the 'for' loop iterations.
-            document.getElementById('scribe4').innerHTML = querynum.concat(cornbrmsg);
-            // Add 1 point to score for correct answer.
-            Count ++;
+            document.getElementById('scribe4').innerHTML = queryNum.concat(corNbrMsg);
+            count ++;
           break;
           }
         }
       };
     //tally up final score
     alert("Game over! ... check your final score below.")
-    document.getElementById('scribe6').innerHTML += (userName+ " ... Your final score is " +Count+ " points out of 5. \nThanks for playing.");
+    document.getElementById('scribe6').innerHTML += (userName+ " ... Your final score is " +count+ " points out of 5. \nThanks for playing.");
   }
 };
